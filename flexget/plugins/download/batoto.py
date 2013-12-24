051fc7bb529c2974ec37f70455f54e435eff3f95
blue <playbynumbers@gmail.com>
2013-12-24 03:40:31 +0000

    Find chapter matching title on series page
    Will now attempt to find the chapter referenced in the entry's title
    if given a series page, obeying any language settings.

--- flexget/plugins/download/batoto.py
+++ flexget/plugins/download/batoto.py
@@ -4,7 +4,10 @@ from os.path import basename, join
 from urlparse import urlparse
 import logging
 from flexget.plugin import register_plugin, priority
-from time import strptime
+import HTMLParser
+from datetime import datetime, timedelta
+import re
+from copy import copy
 
 log = logging.getLogger('batoto')
 
@@ -22,16 +25,31 @@ class Batoto(object):
 	def makesoup(self, url):
 		r = requests.get(url)
 		if r.status_code != 200:
-			log.error(r.status_code + ' error getting ' + r.url)
+			log.error(str(r.status_code) + ' error getting ' + str(r.url))
 			exit(1)
 		return BeautifulSoup(r.text)
+
+	def string_to_time(self, timestring):
+		timestring = timestring.replace('[A]', '')
+		if timestring.find('ago') != -1:
+			value, unit, direction = timestring.split()
+			if value.lower() == 'a': value = 1
+			if not unit.endswith('s'): unit = unit + 's'
+			if direction == 'ago': value *= -1
+			delta = timedelta(**{unit: float(value)})
+			actualtime = datetime.now() + delta
+		else:
+			timestring = timestring.replace('Today,', datetime.now().strftime('%d %B %Y -'))
+			actualtime = datetime.strptime(timestring, '%d %B %Y - %H:%M %p')
+		return actualtime
 
 	def on_task_metainfo(self,task,config):
 		if isinstance(config, bool): config = {}
 		#Should language default to English or None? Unsure. Best option would be get from system locale.
-		#self.language = config.get('language', 'english')
-		self.language = config.get('language')
+		self.language = config.get('language', 'english')
+		#self.language = config.get('language')
 		if isinstance(self.language, basestring): self.language = self.language.title()
+		if self.language == 'None': self.language = None
 		log.debug('Language set to %s', self.language)
 		for entry in task.entries:
 			if entry.get('title'): entry['title'] = entry.get('title').replace('Read Online','').strip()
@@ -52,27 +70,47 @@ class Batoto(object):
 			if urlparse(url)[2].startswith('/comic/_/comics/'):
 				log.verbose('url looks like a series page. Getting most recent upload')
 				soup = self.makesoup(url)
-				rows = soup.find('table', 'chapters_list').findAll('tr','row')
-				newesttime = None
-				newestchapter = None
+				seriesname = soup.find('h1', 'ipsType_pagetitle').text
+				rows = soup.find('table', 'chapters_list').findAll('tr','chapter_row')
+				targetchapter = None
+#				targettime = None
 				for row in rows:
-				    if self.language and not 'lang_' + self.language in row['class'].split(' '): continue
-				    chaptertime = strptime(row.findAll('td')[-1].text, '%d %B %Y - %H:%M %p')
-				    if chaptertime > newesttime:
-				    	newesttime = chaptertime
-				    	newestchapter = row
-				else: entry.fail('Unable to find any chapters matching language %s', self.lang)
-				url = newestchapter.find('a')['href']
-				log.debug('Got url %s' % url)
+					if self.language and not 'lang_' + self.language in row['class'].split(' '): continue
+					parser = copy(entry.get('series_parser'))
+					tds = row.findAll('td')
+					h = HTMLParser.HTMLParser()
+					clean_title = seriesname + ' ' + tds[0].text
+					clean_title = h.unescape(clean_title)
+					clean_title = re.sub('[_.,\[\]\(\):]', ' ', clean_title)
+					parser.parse(clean_title)
+					if parser.pack_identifier == entry.get('series_parser').pack_identifier:
+						targetchapter = row
+						break
+#					chaptertime = self.string_to_time(tds[-1].text)
+#					if not targettime or chaptertime > targettime:
+#						targetchapter = row
+#						targettime = chaptertime
+				if not targetchapter:
+					exitstring = 'Unable to find chapter %s' % entry.get('title')
+					if self.language: exitstring = exitstring + ' in %s' % self.language
+					entry.reject(exitstring)
+					continue
+				else:
+					url = targetchapter.find('a')['href']
+					entry['url'] = url
+					log.debug('Got url %s' % url)
 
-			if not urlparse(url)[2].startswith('/read/'): entry.fail('url is not a chapter page.')
+			if not urlparse(url)[2].startswith('/read/'):
+				entry.reject('url is not a chapter page.')
+				continue
 
 			soup = self.makesoup(url)
 			#try/catch errors here- if find()'s returning None, batoto's probably doing something weird.
 			language = basename(soup.find('select', {'name':'group_select'}).find('option', {'selected':'selected'})['value'])
 			if self.language and language != self.language: entry.fail('Chapter does not match required language.')
-			seriesname = soup.find('div', 'moderation_bar').find('a').text.replace(':','-')
-			chaptername = soup.find('select', {'name':'chapter_select'}).find('option', {'selected':'selected'}).text.replace(':','-')
+			h = HTMLParser.HTMLParser()
+			seriesname = h.unescape(soup.find('div', 'moderation_bar').find('a').text.replace(':','-'))
+			chaptername = h.unescape(soup.find('select', {'name':'chapter_select'}).find('option', {'selected':'selected'}).text.replace(':','-'))
 			pages = soup.find('select', {'name':'page_select'}).findAll('option')
 			log.verbose(seriesname + ' ' + chaptername + ': ' + str(len(pages)) + ' pages')
 
