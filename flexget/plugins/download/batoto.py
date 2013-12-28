ad83e46e6411e1bc60ed9cf44a07c51123d68670
blue <playbynumbers@gmail.com>
2013-12-28 00:27:00 +0000

    Refactor series parsing logic into get_chapter()

--- flexget/plugins/download/batoto.py
+++ flexget/plugins/download/batoto.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 from copy import copy
 import requests
 from BeautifulSoup import BeautifulSoup
-from flexget.plugin import register_plugin, priority, PluginError
+from flexget.plugin import register_plugin, priority, PluginError, PluginWarning
 from flexget.utils.titles import ID_TYPES
 
 log = logging.getLogger('batoto')
@@ -18,21 +18,6 @@ class Batoto(object):
 
 	#This applies to all unexpected behaviour. Remember while troubleshooting.
 	updatewarning = 'If this is unexpected, site may have changed. Plugin may require updating.'
-
-	def string_to_time(self, timestring):
-		timestring = timestring.replace('[A]', '')
-		if timestring.find('ago') != -1:
-			value, unit, direction = timestring.split()
-			if value.lower() == 'a' or value.lower() == 'an': value = float(1)
-			else: value = float(value)
-			if not unit.endswith('s'): unit = unit + 's'
-			if direction == 'ago': value *= -1
-			delta = timedelta(**{unit: value})
-			actualtime = datetime.now() + delta
-		else:
-			timestring = timestring.replace('Today,', datetime.now().strftime('%d %B %Y -'))
-			actualtime = datetime.strptime(timestring, '%d %B %Y - %H:%M %p')
-		return actualtime
 
 	@priority(150)	#Needs to run before series@125
 	def on_task_metainfo(self, task, config):
@@ -81,83 +66,8 @@ class Batoto(object):
 
 			#Are we on a series page? If so, try to get chapter page.
 			if urlparse(r.url)[2].startswith('/comic/_/comics/'):
-				if entry.get('series_parser'): log.verbose('URL looks like a series page. Attempting to get %s'
-					% entry.get('title'))
-				else: log.verbose('URL looks like a series page. Attempting to get most recent chapter.')
-				try:
-					soup = BeautifulSoup(r.text)
-					seriesname = soup.find('h1', 'ipsType_pagetitle').text
-					rows = soup.find('table', 'chapters_list').findAll('tr','chapter_row')
-				except (AttributeError, TypeError) as e:
-					log.error('Encountered an error finding chapters on series page. Site could have been changed, ' +
-						'plugin update may be required.')
-					entry.fail(unicode('Error finding chapters.'))
-					continue
-				except Exception as e:
-					entry.fail(unicode('Error finding chapters. ') + unicode(e))
-					continue
-				parser = copy(entry.get('series_parser'))
-				h = HTMLParser.HTMLParser()
-				targetchapter = None
-				targettime = None
-				targetlanguage = None
-				for row in rows:
-					#Reject anything we can on language & series info
-					if self.language:
-						classes = row['class'].split(' ')
-						language = [language for language in self.language if 'lang_' + language in classes]
-						if not language: continue
-						else:
-							language = language[0]
-							chapterlanguage = self.language.index(language)
-					tds = row.findAll('td')
-					if parser:
-						clean_title = seriesname + ' ' + tds[0].text
-						clean_title = h.unescape(clean_title)
-						clean_title = re.sub('[_.,\[\]\(\):]', ' ', clean_title)
-						parser.parse(clean_title)
-						if parser.pack_identifier == entry.get('series_parser').pack_identifier:
-							log.debug('Chapter match: %s' % clean_title)
-						else: continue
-
-					#See if anything left is a better match than we have
-					chaptertime = self.string_to_time(tds[-1].text)
-					if self.language:
-						log.debug('Chapter language: %s, priority %s' % (language, chapterlanguage))
-						if targetlanguage is not None: log.debug('Chapter conflict: %s(%s) vs %s(%s)'
-							% (language, chapterlanguage, self.language[targetlanguage], targetlanguage))
-						if targetlanguage is None or chapterlanguage < targetlanguage:
-							#lower = listed sooner = higher priority
-							targetlanguage = chapterlanguage
-							targetchapter = row
-							targettime = chaptertime
-							continue
-						elif chapterlanguage == targetlanguage: pass
-						else: continue
-					log.debug('Chapter time: %s' % chaptertime)
-					if targettime is not None: log.debug('Chapter conflict: %s vs %s' % (chaptertime, targettime))
-					if targettime is None or chaptertime > targettime:
-						targetchapter = row
-						targettime = chaptertime
-						if self.language: targetlanguage = chapterlanguage
-				if not targetchapter:
-					exitstring = 'Unable to find chapter %s' % entry.get('title')
-					if self.language:
-						exitstring = exitstring + ' in %s' % self.language
-						entry.reject(unicode(exitstring))
-					else: entry.fail(unicode(exitstring))
-					log.debug(self.updatewarning)
-					continue
-				else:
-					try:
-						url = targetchapter.find('a')['href']
-						entry['url'] = url
-						log.debug('Got url %s' % url)
-						r = requests.get(url)
-						if r.status_code != 200: raise PluginError(str(r.status_code) + ' error getting ' + str(r.url))
-					except Exception as e:
-						entry.fail(unicode(e))
-						continue
+				try: r = self.get_chapter(entry, r)
+				except PluginWarning: continue
 
 			#Are we on a chapter page?
 			if not urlparse(r.url)[2].startswith('/read/'):
@@ -224,5 +134,106 @@ class Batoto(object):
 			entry['download_all'] = True
 		else:
 			if not haveworked: log.error('Encountered no batoto URLs.')
+
+	def get_chapter(self, entry, r):
+		"""Attempts to get a single chapter from a series page, respecting language settings. If a series parser is
+			available, will look for a chapter matching 'title'. If not, it will get the most recent upload.
+
+		:raises: PluginWarning for errors it handles, mostly to allow caller to except PluginWarning: continue
+		"""
+
+		if entry.get('series_parser'): log.verbose('URL looks like a series page. Attempting to get %s'
+			% entry.get('title'))
+		else: log.verbose('URL looks like a series page. Attempting to get most recent chapter.')
+		try:
+			soup = BeautifulSoup(r.text)
+			seriesname = soup.find('h1', 'ipsType_pagetitle').text
+			rows = soup.find('table', 'chapters_list').findAll('tr','chapter_row')
+		except (AttributeError, TypeError) as e:
+			log.error('Encountered an error finding chapters on series page. Site could have been changed, ' +
+				'plugin update may be required.')
+			entry.fail(unicode('Error finding chapters.'))
+			raise PluginWarning('Error encountered while processing %s' % entry.get('title'))
+		except Exception as e:
+			entry.fail(unicode('Error finding chapters. ') + unicode(e))
+			raise PluginWarning('Error encountered while processing %s' % entry.get('title'))
+		parser = copy(entry.get('series_parser'))
+		h = HTMLParser.HTMLParser()
+		targetchapter = None
+		targettime = None
+		targetlanguage = None
+		for row in rows:
+			#Reject anything we can on language & series info
+			if self.language:
+				classes = row['class'].split(' ')
+				language = [language for language in self.language if 'lang_' + language in classes]
+				if not language: continue
+				else:
+					language = language[0]
+					chapterlanguage = self.language.index(language)
+			tds = row.findAll('td')
+			if parser:
+				clean_title = seriesname + ' ' + tds[0].text
+				clean_title = h.unescape(clean_title)
+				clean_title = re.sub('[_.,\[\]\(\):]', ' ', clean_title)
+				parser.parse(clean_title)
+				if parser.pack_identifier == entry.get('series_parser').pack_identifier:
+					log.debug('Chapter match: %s' % clean_title)
+				else: continue
+
+			#See if anything left is a better match than we have
+			chaptertime = self.string_to_time(tds[-1].text)
+			if self.language:
+				log.debug('Chapter language: %s, priority %s' % (language, chapterlanguage))
+				if targetlanguage is not None: log.debug('Chapter conflict: %s(%s) vs %s(%s)'
+					% (language, chapterlanguage, self.language[targetlanguage], targetlanguage))
+				if targetlanguage is None or chapterlanguage < targetlanguage:
+					#lower = listed sooner = higher priority
+					targetlanguage = chapterlanguage
+					targetchapter = row
+					targettime = chaptertime
+					continue
+				elif chapterlanguage == targetlanguage: pass
+				else: continue
+			log.debug('Chapter time: %s' % chaptertime)
+			if targettime is not None: log.debug('Chapter conflict: %s vs %s' % (chaptertime, targettime))
+			if targettime is None or chaptertime > targettime:
+				targetchapter = row
+				targettime = chaptertime
+				if self.language: targetlanguage = chapterlanguage
+		if not targetchapter:
+			exitstring = 'Unable to find chapter %s' % entry.get('title')
+			if self.language:
+				exitstring = exitstring + ' in %s' % self.language
+				entry.reject(unicode(exitstring))
+			else: entry.fail(unicode(exitstring))
+			log.debug(self.updatewarning)
+			raise PluginWarning(exitstring)
+		else:
+			try:
+				url = targetchapter.find('a')['href']
+				entry['url'] = url
+				log.debug('Got url %s' % url)
+				r = requests.get(url)
+				if r.status_code != 200: raise PluginError(str(r.status_code) + ' error getting ' + str(r.url))
+			except Exception as e:
+				entry.fail(unicode(e))
+				raise PluginWarning('Error encountered while processing %s' % entry.get('title'))
+			return r
+
+	def string_to_time(self, timestring):
+		timestring = timestring.replace('[A]', '')
+		if timestring.find('ago') != -1:
+			value, unit, direction = timestring.split()
+			if value.lower() == 'a' or value.lower() == 'an': value = float(1)
+			else: value = float(value)
+			if not unit.endswith('s'): unit = unit + 's'
+			if direction == 'ago': value *= -1
+			delta = timedelta(**{unit: value})
+			actualtime = datetime.now() + delta
+		else:
+			timestring = timestring.replace('Today,', datetime.now().strftime('%d %B %Y -'))
+			actualtime = datetime.strptime(timestring, '%d %B %Y - %H:%M %p')
+		return actualtime
 
 register_plugin(Batoto, 'batoto', api_ver=2)
